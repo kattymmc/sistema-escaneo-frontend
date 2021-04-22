b7cb54cdaad989b186f8e6f8f988c7c203895c19
Katty <katherine.miraval@unmsm.edu.pe>
2021-04-22 18:30:18 -0500

    Mas cambios

--- src/app/core/services/upload.service.ts
+++ src/app/core/services/upload.service.ts
@@ -7,7 +7,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class UploadService {
 
-  private urlEndPoint: string = 'https://salty-plains-51047.herokuapp.com/http://169.57.99.220:32135api/';
+  private urlEndPoint: string = 'https://robwu.nl/http://169.57.99.220:32135/api/';
   private urlHeroku: string = 'https://image-processing-python.herokuapp.com/api/';
 
   constructor(private _http: HttpClient){  }
